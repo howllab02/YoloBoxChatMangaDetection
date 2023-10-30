599d4cddb18db27ea75e08eb8047ce5923f86440
Phòng thí nghiệm của pháp sư Howl <145893992+howllab02@users.noreply.github.com>
2023-10-30 16:52:17 +0700

    Update model.py

--- static/model.py
+++ static/model.py
@@ -7,8 +7,8 @@ class YoloModel:
         self.weight = weight
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', weight)
 
-    def predict(self, bytes_image):
-        results = self.model(bytes_image)
+    def predict(self, tensor_image):
+        results = self.model(tensor_image)
         detections = sv.Detections.from_yolov5(results).with_nms(threshold=0.1)
 
         return detections.xyxy
